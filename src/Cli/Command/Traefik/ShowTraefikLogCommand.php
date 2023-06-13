13a36f4950a8cdd058687325dee8757ece121e1c
Matthias Walter <matthias.walter@mwltr.de>
2023-06-13 13:15:19 +0200

    traefik log with follow option

--- src/Cli/Command/Traefik/ShowTraefikLogCommand.php
+++ src/Cli/Command/Traefik/ShowTraefikLogCommand.php
@@ -5,6 +5,7 @@ namespace RunAsRoot\Rooter\Cli\Command\Traefik;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
@@ -14,19 +15,18 @@ class ShowTraefikLogCommand extends Command
     {
         $this->setName('traefik:log');
         $this->setDescription('Show Traefik logs');
+        $this->addOption('follow', 'f', InputOption::VALUE_NONE, 'follow the log output');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $traefikLog = ROOTER_HOME_DIR . '/traefik/logs/traefik.log';
 
-        $command = "tail -f $traefikLog";
+        $follow = $input->getOption('follow') ? '-f' : '';
 
-        $process = Process::fromShellCommandline($command);
-        $process->setTimeout(0);
-        $process->setTty(true);
-        $process->setOptions(['create_new_console' => 1]);
-        $process->run();
+        $command = "tail $follow $traefikLog";
+
+        Process::fromShellCommandline($command)->setTimeout(0)->setTty(true)->run();
 
         return 0;
     }
