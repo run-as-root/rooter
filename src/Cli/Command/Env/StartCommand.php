1cff9eebe790e5dfb0d439111268ecf3a9620141
Matthias Walter <matthias.walter@mwltr.de>
2023-06-20 09:31:11 +0200

    env:start fixed

--- src/Cli/Command/Env/StartCommand.php
+++ src/Cli/Command/Env/StartCommand.php
@@ -4,28 +4,36 @@ declare(strict_types=1);
 namespace RunAsRoot\Rooter\Cli\Command\Env;
 
 use RunAsRoot\Rooter\Config\DevenvConfig;
-use RunAsRoot\Rooter\Config\RooterConfig;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\ExceptionInterface;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
 class StartCommand extends Command
 {
     private DevenvConfig $devenvConfig;
+    private string $phpBin;
+    private string $phpIniScanDir;
 
     public function configure()
     {
         $this->setName('env:start');
         $this->setDescription('start environment process');
+        $this->addOption('debug','', InputOption::VALUE_NONE, 'activate debug mode');
     }
+
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
         parent::initialize($input, $output);
         $this->devenvConfig = new DevenvConfig();
+        $phpBin = exec('which php');
+        $this->phpBin = realpath($phpBin);
+        $this->phpIniScanDir = dirname($this->phpBin, 2) . "/lib";
     }
+
     /**
      * @throws ExceptionInterface
      */
@@ -47,35 +55,35 @@ class StartCommand extends Command
             throw new \RuntimeException(sprintf('Directory "%s" was not created', ROOTER_PROJECT_DIR));
         }
 
-        $command = sprintf('devenv up > %s 2>&1', $this->devenvConfig->getLogFile());
-
-        // Activate for debugging:
-        // $process = new Process(['devenv', 'up']);
-        // $process->setTty(true);
-
         // Launch the process
-        $process = Process::fromShellCommandline($command);
-        $process->setTimeout(0);
-        $process->setOptions(['create_new_console' => 1]);
+        $debug = $input->getOption('debug');
 
-        $process->start();
+        $command = "devenv up";
+        $process = $this->getProcess($command, $debug);
 
         $output->writeln("<info>environment starting …</info>");
 
-        // Initialize the progress bar
-        $progressBar = $this->getProgressBar($output);
+        if (!$debug) // Initialize the progress bar
+        {
+            $process->start();
 
-        $progressBar->start();
-        while (!$this->hasPid($pidFile)) {
-            usleep(500000); // Sleep for 0.5 seconds
-            $progressBar->advance();
+            $progressBar = $this->getProgressBar($output);
+            $progressBar->start();
+            while (!$this->hasPid($pidFile)) {
+                usleep(500000); // Sleep for 0.5 seconds
+                $progressBar->advance();
+            }
+            $progressBar->finish();
+
+            $output->writeln('');
+
+            $pid = $this->getPidFromFile($pidFile);
+
+            $output->writeln("<info>devenv is running with PID:$pid</info>");
+
+        } else {
+            $process->run();
         }
-        $progressBar->finish();
-        $output->writeln('');
-
-        $pid = $this->getPidFromFile($pidFile);
-
-        $output->writeln("<info>devenv is running with PID:$pid</info>");
 
         return Command::SUCCESS;
     }
@@ -100,5 +108,28 @@ class StartCommand extends Command
         $progressBar->setBarWidth(50);
         $progressBar->setRedrawFrequency(10);
         return $progressBar;
+    }
+
+    private function getProcess(string $command, bool $debug = false): Process
+    {
+        // ROOTER uses a specific PHP version which may not match the one from the env
+        // here me make sure that the correct PHP_BIN and PHP_INI_SCAN_DIR is set
+        // We need to preserve the env from the project and not use rooter env
+        // ROOTER assumes the nginx config has been placed
+        // @todo find a way to call nginx:init here: main question how do we know the envType e.g. magento2 (ENV var?)
+        $command = "export PHP_BIN=\"$this->phpBin\" PHP_INI_SCAN_DIR=\"$this->phpIniScanDir\" ROOTER_INIT_SKIP=1 && " . $command;
+
+        if (!$debug) {
+            $command = sprintf('%s > %s 2>&1', $command, $this->devenvConfig->getLogFile());
+        }
+
+        $process = Process::fromShellCommandline($command, getcwd());
+        $process->setTimeout(0);
+        $process->setOptions(['create_new_console' => 1]);
+        if ($debug) {
+            $process->setTty(true);
+        }
+
+        return $process;
     }
 }
