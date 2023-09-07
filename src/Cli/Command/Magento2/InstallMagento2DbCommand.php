b2a80c8892207b3eb68323429f150748b069a3ec
Matthias Walter <matthias.walter@mwltr.de>
2023-09-07 12:59:25 +0200

    Magento db install 2.4.6 and opensearch compatibility

--- src/Cli/Command/Magento2/InstallMagento2DbCommand.php
+++ src/Cli/Command/Magento2/InstallMagento2DbCommand.php
@@ -5,6 +5,7 @@ namespace RunAsRoot\Rooter\Cli\Command\Magento2;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
@@ -18,6 +19,8 @@ class InstallMagento2DbCommand extends Command
     {
         $this->setName('magento2:db-install');
         $this->setDescription('Initialise fresh database');
+        $this->addOption('skip-reindex', '', InputOption::VALUE_NONE, 'skip reindex after importing the dump');
+        $this->addOption('skip-config-import', '', InputOption::VALUE_NONE, 'skip config import after installation');
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output)
@@ -29,6 +32,9 @@ class InstallMagento2DbCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $skipConfigImport = $input->getOption('skip-config-import');
+        $skipReindex = $input->getOption('skip-reindex');
+
         $DEVENV_DB_USER = getenv('DEVENV_DB_USER');
         $DEVENV_DB_PASS = getenv('DEVENV_DB_PASS');
         $DEVENV_DB_PORT = getenv('DEVENV_DB_PORT');
@@ -36,6 +42,7 @@ class InstallMagento2DbCommand extends Command
         $PROJECT_HOST = getenv('PROJECT_HOST');
         $DEVENV_REDIS_PORT = getenv('DEVENV_REDIS_PORT');
         $DEVENV_ELASTICSEARCH_PORT = getenv('DEVENV_ELASTICSEARCH_PORT');
+        $DEVENV_OPENSEARCH_PORT = getenv('DEVENV_OPENSEARCH_PORT');
         $DEVENV_AMQP_PORT = getenv('DEVENV_AMQP_PORT');
         $DEVENV_AMQP_USER = getenv('DEVENV_AMQP_USER');
         $DEVENV_AMQP_PASS = getenv('DEVENV_AMQP_PASS');
@@ -63,22 +70,33 @@ class InstallMagento2DbCommand extends Command
                 --page-cache-redis-server=127.0.0.1 \
                 --page-cache-redis-db=1 \
                 --page-cache-redis-port=$DEVENV_REDIS_PORT  \
-                --elasticsearch-host=\"127.0.0.1\" \
-                --elasticsearch-port=\"$DEVENV_ELASTICSEARCH_PORT\" \
-                --elasticsearch-username=\"\" \
-                --elasticsearch-password=\"\" \
                 --amqp-host=127.0.0.1 --amqp-port=$DEVENV_AMQP_PORT --amqp-user=$DEVENV_AMQP_USER --amqp-password=$DEVENV_AMQP_PASS --amqp-virtualhost=\"/\"";
+
+        if ($DEVENV_ELASTICSEARCH_PORT) {
+            $command .= " \
+                --search-engine=elasticsearch7 \
+                --elasticsearch-host=\"127.0.0.1\" --elasticsearch-port=\"$DEVENV_ELASTICSEARCH_PORT\" --elasticsearch-username=\"\" --elasticsearch-password=\"\"";
+        } elseif ($DEVENV_OPENSEARCH_PORT) {
+            $command .= " \
+                --search-engine=opensearch \
+                --opensearch-host=\"127.0.0.1\" --opensearch-port=\"$DEVENV_OPENSEARCH_PORT\" --opensearch-username=\"\" --opensearch-password=\"\"";
+        }
+
         $this->runCommand($command);
 
         $command = "$this->phpBin bin/magento setup:upgrade";
         $this->runCommand($command);
 
-        $command = "$this->phpBin bin/magento config:data:import config/store dev/rooter";
-        $this->runCommand($command);
+        if(!$skipConfigImport) {
+            $command = "$this->phpBin bin/magento config:data:import config/store dev/rooter";
+            $this->runCommand($command);
+        }
 
         // Reindex so Elasticsearch gets the updated data
-        $command = "$this->phpBin bin/magento indexer:reindex";
-        $this->runCommand($command);
+        if (!$skipReindex) {
+            $command = "$this->phpBin bin/magento indexer:reindex";
+            $this->runCommand($command);
+        }
 
         return 0;
     }
