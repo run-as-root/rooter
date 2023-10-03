e34466e212e6c2f885343c538f9da02b3216990e
Matthias Walter <matthias.walter@mwltr.de>
2023-10-03 12:58:49 +0200

    ADD ProcessCompose ProcessList to ShowEnvCommand

--- src/Cli/Command/Env/ShowEnvCommand.php
+++ src/Cli/Command/Env/ShowEnvCommand.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace RunAsRoot\Rooter\Cli\Command\Env;
 
+use RunAsRoot\Rooter\Api\ProcessCompose\Exception\ApiException;
+use RunAsRoot\Rooter\Api\ProcessCompose\ProcessComposeApi;
 use RunAsRoot\Rooter\Repository\EnvironmentRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
@@ -12,8 +14,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ShowEnvCommand extends Command
 {
-    public function __construct(private readonly EnvironmentRepository $envRepository)
-    {
+    public function __construct(
+        private readonly EnvironmentRepository $envRepository,
+        private readonly ProcessComposeApi $processComposeApi
+    ) {
         parent::__construct();
     }
 
@@ -28,7 +32,17 @@ class ShowEnvCommand extends Command
     {
         $projectName = $input->getArgument('name') ?? getenv('PROJECT_NAME');
 
-        $envData = $this->envRepository->getByName($projectName);
+        if (!$projectName) {
+            $output->writeln("<error>Please provide a project-name or execute in a project context.</error>");
+            return Command::FAILURE;
+        }
+
+        try {
+            $envData = $this->envRepository->getByName($projectName);
+        } catch (\JsonException $e) {
+            $output->writeln("<error>Could not get environment config: invalid json {$e->getMessage()}</error>");
+            return Command::FAILURE;
+        }
 
         $attributes = [
             'name',
@@ -49,7 +63,6 @@ class ShowEnvCommand extends Command
 
         $table = new Table($output);
         $table->setStyle('box');
-
         $table->setHeaderTitle('Environment Variables');
         $table->setHeaders(['Variable', 'Value']);
 
@@ -62,7 +75,38 @@ class ShowEnvCommand extends Command
 
         $table->render();
 
+        $this->renderEnvironmentProcessList($envData, $output);
+
         return Command::SUCCESS;
+    }
+
+    private function renderEnvironmentProcessList(array $envData, OutputInterface $output): void
+    {
+        try {
+            $this->processComposeApi->isAlive($envData);
+
+            $processData = $this->processComposeApi->getProcessList($envData);
+        } catch (ApiException $e) {
+            $output->writeln("<error>{$e->getMessage()}</error>");
+            return;
+        } catch (\JsonException $e) {
+            $output->writeln("<error>Could not parse json. Invalid json response from process-compose: {$e->getMessage()}</error>");
+            return;
+        } catch (\Exception $e) {
+            $output->writeln("<error>Error fetching data from process-compose: {$e->getMessage()}</error>");
+            return;
+        }
+
+        $table = new Table($output);
+        $table->setStyle('box');
+        $table->setHeaderTitle('Process Overview');
+        $table->setHeaders(['Name', 'PID', 'uptime', 'Status']);
+
+        foreach ($processData as $process) {
+            $table->addRow([$process['name'], $process['pid'], $process['system_time'], $process['status']]);
+        }
+
+        $table->render();
     }
 }
 
