c2ce4c4b96b6c626090a6016ca341434b275b0c1
Matthias Walter <matthias.walter@mwltr.de>
2023-10-17 10:15:41 +0200

    add command to run devenv update

---
+++ src/Cli/Command/Devenv/UpdateDevenvCommand.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace RunAsRoot\Rooter\Cli\Command\Devenv;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UpdateDevenvCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('devenv:update');
+        $this->setDescription('run devenv update for the current environment');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        shell_exec("devenv update");
+
+        return Command::SUCCESS;
+    }
+}
