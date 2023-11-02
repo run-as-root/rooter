63663eada10132b8c96e4dd00c3f1d0b6c102974
Matthias Walter <matthias.walter@mwltr.de>
2023-11-02 13:44:12 +0100

    add php-plain template

---
+++ environments/php-plain/devenv.nix
@@ -0,0 +1,42 @@
+{ pkgs, inputs, lib, config, ... }:
+let
+
+in {
+    dotenv.enable = true;
+    env = {
+        PROJECT_NAME = "${PROJECT_NAME}";
+        PROJECT_HOST = "${PROJECT_HOST}";
+    };
+
+    # PACKAGES
+    packages = [
+        pkgs.git
+        pkgs.gnupatch
+        pkgs.curl
+        pkgs.yarn
+        pkgs.gettext
+    ];
+
+    # PHP
+    languages.php = {
+        enable = true;
+        package = inputs.phps.packages.${builtins.currentSystem}.php82.buildEnv {
+            extensions = { all, enabled }: with all; enabled ++ [ redis xdebug xsl ];
+            extraConfig = ''
+                memory_limit = -1
+                error_reporting=E_ALL
+                xdebug.mode = coverage,debug
+                sendmail_path = ${pkgs.mailpit}/bin/mailpit sendmail -S 127.0.0.1:${config.env.DEVENV_MAIL_SMTP_PORT}
+                display_errors = On
+                display_startup_errors = On
+            '';
+        };
+    };
+
+    # mailpit
+    services.mailpit = {
+        enable = true;
+        uiListenAddress   = "127.0.0.1:${config.env.DEVENV_MAIL_UI_PORT}";
+        smtpListenAddress = "127.0.0.1:${config.env.DEVENV_MAIL_SMTP_PORT}";
+    };
+}
