4db4a0765d96bfebbe5265b506c71d47d44126c6
Matthias Walter <matthias.walter@mwltr.de>
2024-06-11 14:49:34 +0200

    add PWA-Studio environment template

---
+++ environments/pwa-studio/devenv.nix
@@ -0,0 +1,37 @@
+{ pkgs, inputs, lib, config, ... }:
+let
+
+in {
+    dotenv.enable = true;
+    env = {
+        PROJECT_NAME = "${PROJECT_NAME}";
+        PROJECT_HOST = "${PROJECT_HOST}";
+
+        PC_SOCKET_PATH = "${config.env.DEVENV_STATE}/process-compose.sock";
+    };
+
+    # PACKAGES
+    packages = [
+        pkgs.git
+        pkgs.gnupatch
+        pkgs.curl
+        pkgs.yarn
+        pkgs.gettext
+    ];
+
+    # process-compose
+    process.implementation="process-compose";
+
+    # JS
+    languages.javascript = {
+        enable = true;
+        package = pkgs.nodejs_20;
+    };
+
+    # mailpit
+    services.mailpit = {
+        enable = true;
+        uiListenAddress   = "127.0.0.1:${config.env.DEVENV_MAIL_UI_PORT}";
+        smtpListenAddress = "127.0.0.1:${config.env.DEVENV_MAIL_SMTP_PORT}";
+    };
+}
